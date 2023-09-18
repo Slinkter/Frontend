845fd10a0831bfdc09a768f1c217e3122f794787
Slinkter <luis.j.cueva@gmail.com>
2023-09-18 10:02:48 -0500

    .

--- 02.Javascript-a-Profundidad/09.Curso-de-manI-de-arrays-fin/App/00-Test.js
+++ 02.Javascript-a-Profundidad/09.Curso-de-manI-de-arrays-fin/App/00-Test.js
@@ -37,4 +37,34 @@ const orders = [
     delivered: true,
   },
 ];
+const newOrdersTotal = orders.map((elemento) => elemento.total);
+const newOrdersIGV = orders.map((elemento) =>
+  Math.round(elemento.total * 1.18)
+);
+const newOrdersAdd = orders.map((element) => {
+  return { ...element, clave: "valor" };
+});
 console.log(orders);
+console.log(newOrdersTotal);
+console.log(newOrdersIGV);
+console.log(newOrdersAdd);
+const products = [
+  {
+    name: "Product 1",
+    price: 1000,
+    stock: 10,
+  },
+  {
+    name: "Product 2",
+    price: 2000,
+    stock: 20,
+  },
+];
+const newProducts = products.map((product) => {
+  return {
+    ...product,
+    taxes: Math.trunc(product.price * 0.18),
+    taxes2: product.price * 0.18,
+  };
+});
+console.log(newProducts);
