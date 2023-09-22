66cb68c4db6fa9ec2e785fabcfd97bb4125c110c
Slinkter <luis.j.cueva@gmail.com>
2023-09-22 10:07:52 -0500

    rama javascript

--- 02.Javascript-a-Profundidad/05.Curso-de-closures-scope-js/App/function.js
+++
@@ -1,8 +0,0 @@
-/* Clase 03 */
-function greeting() {
-  let username = "Ana";
-  if (username === "Ana") {
-    console.log(`hello ${username}`);
-  }
-}
-greeting();
--- 02.Javascript-a-Profundidad/05.Curso-de-closures-scope-js/App/strict.js
+++
@@ -1,5 +0,0 @@
-/* Clase 06 */
-"use strict";
-// var pi;
-pi = 3.1416;
-console.log(pi);
