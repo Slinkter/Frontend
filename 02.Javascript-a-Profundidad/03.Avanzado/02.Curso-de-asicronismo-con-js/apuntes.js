53fffb7acbcbf075a6bd542922d28d41f2b795bf
Slinkter <luis.j.cueva@gmail.com>
2023-09-22 19:17:42 -0500

    clase 6/26 : curso de asicronimos con JS

---
+++ 02.Javascript-a-Profundidad/03.Avanzado/02.Curso-de-asicronismo-con-js/apuntes.js
@@ -0,0 +1,94 @@
+/* Clase 01 */
+// introduccion
+/* Clase 02  */
+/* 
+
+QUE ES ASINCRONISMO ?
+
+Los lenguajes de programacion son sincronicos proque tienen que ejecutar
+tarea por tareas, en si JS es sincrono y utiliza un subproceso como hilo.
+
+Js es single-threaded
+
+Js procesa en un solo hilo , 
+
+Js es Bloqueante 
+
+una tarea puede ser bloqueada hasta que termine . por ejemplo un alert 
+la aplicacion no puede avanzar hasta atender ese alert 
+
+Js es no bloquenate
+
+un tareas se resolvie rapido independiente del resultado 
+
+JS es Sincrono
+
+las tareas se ejecunta de manera secuencial. las tareas debe esperar hasta q termine la
+tarea anterior
+
+Js es asincrono
+
+las tareas puede ser resultadas mas tarde para que otras tareas se pueden resolver
+es decirl el resultado es diferido a otras tareas.
+
+Js es Concurencia
+
+eventos basado en eventloop  
+
+Js es EventLoop
+
+que es un patro de diseño que esperar 
+y distribuye los eventos
+
+*** Forma de procesar o manejar Asicronia 
+
+****  Callback
+es una funcion, que es parte un argumento de una funcion , que sera incovacada
+****  Promesas (ES6)
+es una funcion no-bloqueante asicronia ,puede cumplir una promesa ahora , futuro o nunca .
+**** Async / Await (ES2017)
+es una funciona asincrona sin bloqueo , solo es sintaxis mas amigable
+
+con estas formas js puede simular multi-hilo por estas herramientas
+
+
+Js es un lenguaje de programacion no bloqueante asicrono donde existe el bucle de un solo
+hilo para interfaces I/O 
+
+
+*/
+
+/* 
+Clase 03
+
+*** Event Loop
+es un patron de diseño se distribuye eventos o mensaje de un programa
+es un bucle , funciona solo si el stack esta vacio para que el task queue pueda mover las tareas
+ya realizadas 
+*** Memory Heap 
+es un recurso , espacio en memoria donde los elementos estan asignados
+*** Call Stack
+Apila y organiza las instrucciones de un programa 
+*** Task queue
+Maneja las concurrencias 
+solo se agrega las tareas que ya esta listas para pasar al stack
+*** MicroTask Queue
+tiene prioridad superior a resolver como promesas
+*** Web APIs 
+js y node , manipulacion de archivos del os y peticiones Http
+(imagen)
+http://latentflip.com/loupe/?code=JC5vbignYnV0dG9uJywgJ2NsaWNrJywgZnVuY3Rpb24gb25DbGljaygpIHsKICAgIHNldFRpbWVvdXQoZnVuY3Rpb24gdGltZXIoKSB7CiAgICAgICAgY29uc29sZS5sb2coJ1lvdSBjbGlja2VkIHRoZSBidXR0b24hJyk7ICAgIAogICAgfSwgMjAwMCk7Cn0pOwoKY29uc29sZS5sb2coIkhpISIpOwoKc2V0VGltZW91dChmdW5jdGlvbiB0aW1lb3V0KCkgewogICAgY29uc29sZS5sb2coIkNsaWNrIHRoZSBidXR0b24hIik7Cn0sIDUwMDApOwoKY29uc29sZS5sb2coIldlbGNvbWUgdG8gbG91cGUuIik7!!!PGJ1dHRvbj5DbGljayBtZSE8L2J1dHRvbj4%3D
+
+
+*/
+/* Clase 05.
+configuraion vscode code run
+*/
+/* Clase 06 */
+/* 
+callback
+
+
+
+
+*/
---
+++ 02.Javascript-a-Profundidad/03.Avanzado/02.Curso-de-asicronismo-con-js/src/index.js
@@ -0,0 +1,12 @@
+function sum(num1, num2) {
+  return num1 + num2;
+}
+function calc(num1, num2, callback) {
+  return callback(num1, num2);
+}
+
+console.log(calc(2, 9, sum));
+
+setTimeout(() => {
+  console.log("hola JS");
+}, 2000);
