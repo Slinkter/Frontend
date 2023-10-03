aab22ed8b4130aebbf3c09162bd8465e3dd44315
Slinkter <luis.j.cueva@gmail.com>
2023-10-02 19:09:48 -0500

    macos curso api rest :apis reales

--- 02.Javascript-a-Profundidad/03.Avanzado/04.Curso-de-Api-Rest-real/src/main.js
+++ 02.Javascript-a-Profundidad/03.Avanzado/04.Curso-de-Api-Rest-real/src/main.js
@@ -1,7 +1,6 @@
-console.log("main.js  " + API_KEY);
-
 async function getTrendingMoviesPreview() {
-  const url = `https://api.themoviedb.org/3/trending/movie/day?language=en-US`;
+  /*  */
+  const url = `https://api.themoviedb.org/3/trending/movie/day`;
   const options = {
     method: "GET",
     headers: {
@@ -10,12 +9,81 @@ async function getTrendingMoviesPreview() {
         "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YWI3MTYzMjk1NzE0NmVlNGI3ZjNkZWFlMWRjMzM1NSIsInN1YiI6IjY1MTQzY2RmZWE4NGM3MDEwYzEwZTc1MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1Mq4jS2yqZX6vbG28UgpsCJujgEYbb66Vrz07_2VwlY",
     },
   };
+  /*  */
+  try {
+    const res = await fetch(url, options);
+    const data = await res.json();
+    const movies = data.results;
+    // article selected
+    const trendingPreviewMoviesContainer = document.querySelector(
+      "#trendingPreview .trendingPreview-movieList"
+    );
 
-  const res = await fetch(url, options);
-  const data = await res.json();
-  console.log(data);
-  console.log(data.results);
+    movies.map((movie) => {
+      /*  */
+      const movieContainer = document.createElement("div"); // create
+      movieContainer.classList.add("movie-container");
+      /*  */
+      const movieImg = document.createElement("img"); // create
+      movieImg.classList.add("movie-img");
+      movieImg.setAttribute("alt", movie.title);
+      movieImg.setAttribute(
+        "src",
+        "https://image.tmdb.org/t/p/w300/" + movie.poster_path
+      );
+      /*  */
+      movieContainer.appendChild(movieImg); // add
+      trendingPreviewMoviesContainer.appendChild(movieContainer); // add
+    });
+
+    console.log(data);
+    console.log(data.results);
+  } catch (error) {
+    console.error("error", error);
+  }
   //return data;
 }
 
+async function getCategoriesPreview() {
+  /*  */
+  const url = `https://api.themoviedb.org/3/genre/movie/list?language=en`;
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YWI3MTYzMjk1NzE0NmVlNGI3ZjNkZWFlMWRjMzM1NSIsInN1YiI6IjY1MTQzY2RmZWE4NGM3MDEwYzEwZTc1MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1Mq4jS2yqZX6vbG28UgpsCJujgEYbb66Vrz07_2VwlY",
+    },
+  };
+  /*  */
+  try {
+    const res = await fetch(url, options);
+    const data = await res.json();
+    const categories = data.genres;
+    /*  */
+
+    categories.forEach((category) => {
+      const previewCategoriesContainer = document.querySelector(
+        "#categoriesPreview .categoriesPreview-list"
+      );
+      const categoryContainer = document.createElement("div");
+      categoryContainer.classList.add("category-container");
+      /*  */
+      const categoryTitle = document.createElement("h3");
+      categoryTitle.classList.add("category-title");
+      categoryTitle.setAttribute("id", "id" + category.id);
+      const categoryTitleText = document.createTextNode(category.name);
+      categoryTitle.appendChild(categoryTitleText);
+      categoryContainer.appendChild(categoryTitle);
+      previewCategoriesContainer.appendChild(categoryContainer);
+    });
+
+    console.log(data);
+    console.log(categories);
+  } catch (error) {
+    console.error("error", error);
+  }
+}
+
 getTrendingMoviesPreview();
+getCategoriesPreview();
