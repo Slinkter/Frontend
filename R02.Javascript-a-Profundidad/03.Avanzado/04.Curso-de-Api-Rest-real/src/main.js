38b7745f36d506d5ea88fa8d3792706453225496
Slinkter <luis.j.cueva@gmail.com>
2023-10-04 19:03:24 -0500

    ok

--- R02.Javascript-a-Profundidad/03.Avanzado/04.Curso-de-Api-Rest-real/src/main.js
+++ R02.Javascript-a-Profundidad/03.Avanzado/04.Curso-de-Api-Rest-real/src/main.js
@@ -1,4 +1,5 @@
-/* clase 11/17 : Filtrando películas por categoría
+/* 
+clase 11/17 : Filtrando películas por categoría
  */
 
 async function getTrendingMoviesPreview() {
@@ -81,10 +82,55 @@ async function getCategoriesPreview() {
       categoryTitle.appendChild(categoryTitleText);
       categoryContainer.appendChild(categoryTitle);
       categoriesPreviewList.appendChild(categoryContainer);
+      /* cuando hace click */
+      categoryTitle.addEventListener("click", () => {
+        location.hash = "#category=" + category.id + "-" + category.name;
+      });
     });
   } catch (error) {
     console.error("error", error);
   }
 }
+
+async function getMoviesByCategory(id) {
+  console.log("getMoviesByCategory(id)");
+  /*  */
+  const url = `https://api.themoviedb.org/3/discover/movie?with_genres=${id}`;
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YWI3MTYzMjk1NzE0NmVlNGI3ZjNkZWFlMWRjMzM1NSIsInN1YiI6IjY1MTQzY2RmZWE4NGM3MDEwYzEwZTc1MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1Mq4jS2yqZX6vbG28UgpsCJujgEYbb66Vrz07_2VwlY",
+    },
+  };
+  /*  */
+  try {
+    const res = await fetch(url, options);
+    const data = await res.json();
+    const movies = data.results;
+    console.log("data: ", data);
+    console.log("results: ", movies);
+
+    genericSection.innerHTML = "";
+    movies.forEach((movie) => {
+      const movieContainer = document.createElement("div");
+      const movieImg = document.createElement("img");
+      //
+      movieContainer.classList.add("movie-container");
+      movieImg.classList.add("movie-img");
+      const url = "https://image.tmdb.org/t/p/w300/" + movie.poster_path;
+      const title = movie.title;
+      movieImg.setAttribute("src", url);
+      movieImg.setAttribute("alt", title);
+      movieContainer.appendChild(movieImg);
+      genericSection.appendChild(movieContainer);
+    });
+  } catch (error) {
+    console.error("error", error);
+  }
+  //return data;
+}
+
 getTrendingMoviesPreview();
 getCategoriesPreview();
