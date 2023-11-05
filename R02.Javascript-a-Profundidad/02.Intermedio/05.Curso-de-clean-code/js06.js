513878e05b421a85a48da0b46fd7564a398cec32
Slinkter <luis.j.cueva@gmail.com>
2023-11-05 10:23:00 -0500

    win 11

---
+++ R02.Javascript-a-Profundidad/02.Intermedio/05.Curso-de-clean-code/js06.js
@@ -0,0 +1,38 @@
+/* 
+Arrays: Plural.
+Booleanos: Con prefijos “is”, “has” y “can”. (“es verdadero”, “tiene/contiene x”, “puede hacer x”).
+Números: añadir  prefijos “min”, “max” y “total”.
+Funciones: representan un accion ,
+    Verbo + sustantivo/s (ya que son acciones).
+    …de Acceso: get + sustantivo
+    …de Modificación: set + sustantivo;
+    …de Predicado: is + sustantivo;
+
+Clases: Sustantivos (no genéricos).
+*/
+
+// Arrays:
+const users = ["Alex", "Mariana", "Jessica"];
+
+//Booleanos:
+const isValid = true;
+const hasColor = true;
+const canRead = false;
+
+//Números:
+const maxUsers = 50;
+const minUsers = 10;
+const totalUsers = 15;
+
+//Funciones:
+createUser(); // verb : create
+updateUser(); // verb : update
+sendEmail(); // verb : send
+getUser(); //Acceso:
+setUser(); //Modificación:
+isValidUser(); //Predicado:
+
+//Clases:
+class User {}
+class UserProfile {}
+class Account {}
