137c17b4277b5c101de75acf4b98d681f1851302
Slinkter <luis.j.cueva@gmail.com>
2023-11-12 20:56:25 -0500

    macos

--- Practicando/index.js
+++ Practicando/index.js
@@ -1,3 +1,7 @@
-const numbers = [1, 2, 3, 4, 5];
-const doble = numbers.reduce((acc, current) => acc.concat(current * 2), []);
-console.log(doble);
+const promesa1 = Promise.reject("Ups promesa 1 falló");
+const promesa2 = Promise.reject("Ups promesa 2 falló");
+const promesa3 = Promise.resolve("Promesa 3");
+
+Promise.any([promesa1, promesa2, promesa3])
+  .then((respuesta) => console.log(respuesta))
+  .catch((error) => console.log(error));
--- R02.Javascript-a-Profundidad/01.Basico/05.Curso-de-closures-scope/07-mymoneybox.js
+++ R02.Javascript-a-Profundidad/01.Basico/05.Curso-de-closures-scope/07-mymoneybox.js
@@ -26,3 +26,19 @@ const rpta2 = moneyBoxClosure();
 rpta2(100);
 rpta2(15);
 rpta2(25);
+
+/* 
+Explicacion 
+
+
+La razón por la que la función moneyBox no funciona es porque 
+la variable "saveCoins" NO mantiene su valor entre llamadas a la función. 
+Esto se debe a que saveCoins es una variable local, y las variables locales se crean y destruyen cada vez que se llama a la función.
+
+En cambio, la función moneyBoxClosure funciona porque crea una closure. Una closure es una función que recuerda el valor de sus variables locales incluso después de que la función haya terminado de ejecutarse. Esto se debe a que la closure tiene acceso a un entorno léxico especial que contiene los valores de las variables locales de la función.
+
+En el caso de la función moneyBoxClosure, la closure recuerda el valor de la variable saveCoins incluso después de que se haya terminado de ejecutar la función moneyBoxClosure. Esto significa que el valor de saveCoins se mantiene entre llamadas a la función countCoins, lo que permite que la función countCoins realice un seguimiento de la cantidad total de monedas que se han guardado en la hucha.
+
+Por lo tanto, la función moneyBoxClosure es una forma más efectiva de crear una hucha porque mantiene un registro de la cantidad total de monedas que se han guardado en la hucha, incluso después de que se haya terminado de ejecutar la función.
+
+*/
