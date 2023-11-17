9c5a79774c155726f962fff77ddf5151673514df
Slinkter <luis.j.cueva@gmail.com>
2023-11-16 19:17:09 -0500

    win11

--- R02.Javascript-a-Profundidad/03.Avanzado/04.Curso-de-Api-Rest-real-fin/src/main.js
+++ R02.Javascript-a-Profundidad/03.Avanzado/04.Curso-de-Api-Rest-real-fin/src/main.js
@@ -20,16 +20,16 @@ async function getTrendingMoviesPreview() {
       /* create elements*/
       const movieContainer = document.createElement("div");
       const movieImg = document.createElement("img");
+      const url = "https://image.tmdb.org/t/p/w300/" + movie.poster_path;
+      const title = movie.title;
       /* Add style */
       movieContainer.classList.add("movie-container");
+
       movieContainer.addEventListener("click", () => {
         location.hash = "#movie=" + movie.id;
       });
-
       movieImg.classList.add("movie-img");
       /* Add url img */
-      const url = "https://image.tmdb.org/t/p/w300/" + movie.poster_path;
-      const title = movie.title;
       movieImg.setAttribute("src", url);
       movieImg.setAttribute("alt", title);
       /* Add html */
@@ -189,53 +189,6 @@ async function getMoviesBySearch(query) {
   console.groupEnd("");
 }
 
-async function getTrendingMovies() {
-  try {
-    // params
-    const url = `https://api.themoviedb.org/3/trending/movie/day`;
-    const authAPI =
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YWI3MTYzMjk1NzE0NmVlNGI3ZjNkZWFlMWRjMzM1NSIsInN1YiI6IjY1MTQzY2RmZWE4NGM3MDEwYzEwZTc1MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1Mq4jS2yqZX6vbG28UgpsCJujgEYbb66Vrz07_2VwlY";
-    const options = {
-      method: "GET",
-      headers: {
-        accept: "application/json",
-        Authorization: authAPI,
-      },
-    };
-    // call api
-    const res = await fetch(url, options);
-    const data = await res.json();
-    const movies = data.results;
-    // Add HTML
-    genericSection.innerHTML = "";
-    movies.map((movie) => {
-      /* create elements*/
-      const movieContainer = document.createElement("div");
-      const movieImg = document.createElement("img");
-      /* Add style */
-      movieContainer.classList.add("movie-container");
-      movieContainer.addEventListener("click", () => {
-        location.bash = "#movie=" + movie.id;
-      });
-
-      movieImg.classList.add("movie-img");
-
-      /* Add url img */
-      const url = "https://image.tmdb.org/t/p/w300/" + movie.poster_path;
-      const title = movie.title;
-      movieImg.setAttribute("src", url);
-      movieImg.setAttribute("alt", title);
-      /* Add html */
-      movieContainer.appendChild(movieImg);
-      genericSection.appendChild(movieContainer);
-    });
-  } catch (error) {
-    console.error("error", error);
-  } finally {
-    console.log("finally : getTrendingMoviesPreview()");
-  }
-}
-
 async function getMovieBySearch(movieId) {
   try {
     // params
@@ -355,6 +308,52 @@ async function getRelatedMoviesId(id) {
     movieContainer.appendChild(movieImg);
     relatedMoviesContainer.appendChild(movieContainer);
   });
+}
+async function getTrendingMovies() {
+  try {
+    // params
+    const url = `https://api.themoviedb.org/3/trending/movie/day`;
+    const authAPI =
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YWI3MTYzMjk1NzE0NmVlNGI3ZjNkZWFlMWRjMzM1NSIsInN1YiI6IjY1MTQzY2RmZWE4NGM3MDEwYzEwZTc1MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1Mq4jS2yqZX6vbG28UgpsCJujgEYbb66Vrz07_2VwlY";
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: authAPI,
+      },
+    };
+    // call api
+    const res = await fetch(url, options);
+    const data = await res.json();
+    const movies = data.results;
+    // Add HTML
+    genericSection.innerHTML = "";
+    movies.map((movie) => {
+      /* create elements*/
+      const movieContainer = document.createElement("div");
+      const movieImg = document.createElement("img");
+      /* Add style */
+      movieContainer.classList.add("movie-container");
+      movieContainer.addEventListener("click", () => {
+        location.bash = "#movie=" + movie.id;
+      });
+
+      movieImg.classList.add("movie-img");
+
+      /* Add url img */
+      const url = "https://image.tmdb.org/t/p/w300/" + movie.poster_path;
+      const title = movie.title;
+      movieImg.setAttribute("src", url);
+      movieImg.setAttribute("alt", title);
+      /* Add html */
+      movieContainer.appendChild(movieImg);
+      genericSection.appendChild(movieContainer);
+    });
+  } catch (error) {
+    console.error("error", error);
+  } finally {
+    console.log("finally : getTrendingMoviesPreview()");
+  }
 }
 
 getTrendingMoviesPreview();
