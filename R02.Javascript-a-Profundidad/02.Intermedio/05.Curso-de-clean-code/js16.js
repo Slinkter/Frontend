80005f38474501862262c86750137f5873f4cf89
Slinkter <luis.j.cueva@gmail.com>
2023-11-18 16:54:27 -0500

    Clases

---
+++ R02.Javascript-a-Profundidad/02.Intermedio/05.Curso-de-clean-code/js16.js
@@ -0,0 +1,24 @@
+/* Clases */
+
+//Antes dee ES6
+function Person(name) {
+  this.name = name;
+}
+Person.prototype.greet = function () {
+  return "hello I am " + this.name;
+};
+
+// Con Es6
+
+class Cat {
+  constructor(cat) {
+    this.name = cat;
+  }
+  greet() {
+    return "hello I am " + this.name;
+  }
+}
+
+const cat = new Cat("Oliver");
+console.log(cat.name);
+console.log(cat.greet());
