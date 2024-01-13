8a310f7de6c7ffa9dbb864c79423b5138e4c0bf3
Slinkter <luis.j.cueva@gmail.com>
2024-01-12 19:37:57 -0500

    update macos

--- R04.Fronted-con-reacts/React-Platzi/src/01.Ruta-01/01.Curso-de-Introduccion-react/components/Modal/index.js
+++ R04.Fronted-con-reacts/React-Platzi/src/01.Ruta-01/01.Curso-de-Introduccion-react/components/Modal/index.js
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import './modal.css'
+import "./modal.css";
 
 function Modal({ children }) {
   return ReactDOM.createPortal(
-    <div className="ModalBackground">
-    {children}
-    </div>,
+    <div className="ModalBackground">{children}</div>,
+
     document.getElementById("modal")
   );
 }
