1054879376ad665e09db4d7225fb47e97a17dba4
Slinkter <luis.j.cueva@gmail.com>
2024-01-29 17:58:04 -0500

    ok

--- R04.Fronted-con-reacts/react_tutorial_interm/src/tutorial04/ClassYoutube/Class11/HigherOrder.js
+++ R04.Fronted-con-reacts/react_tutorial_interm/src/tutorial04/ClassYoutube/Class11/HigherOrder.js
@@ -1,28 +1,29 @@
 import { useEffect, useState } from "react";
 
 function HigherOrder(title, Component, nameRequest) {
-    return function HOC() {
-        // hooks
-        const [data, setData] = useState([]);
-        // servidor
-        const getData = async () => {
-            const url = `https://jsonplaceholder.typicode.com/${nameRequest}`;
-            const res = await fetch(url);
-            const data = await res.json();
-            setData(data);
-        };
-        // cycle data
-        useEffect(() => {
-            getData();
-        }, []);
-        // -->
-        return (
-            <div>
-                <h2>{title}</h2>
-                <Component data={data} />
-            </div>
-        );
+  return function HOC() {
+    // URL
+    const url_api = `https://jsonplaceholder.typicode.com/${nameRequest}`;
+    // hooks
+    const [data, setData] = useState([]);
+    // servidor
+    const getData = async () => {
+      const res = await fetch(url_api);
+      const data = await res.json();
+      setData(data);
     };
+    // cycle data
+    useEffect(() => {
+      getData();
+    }, []);
+    // render
+    return (
+      <div>
+        <h2>{title}</h2>
+        <Component data={data} />
+      </div>
+    );
+  };
 }
 
 export default HigherOrder;
--- R04.Fronted-con-reacts/react_tutorial_interm/src/tutorial04/ClassYoutube/Class12/Class12.jsx
+++ R04.Fronted-con-reacts/react_tutorial_interm/src/tutorial04/ClassYoutube/Class12/Class12.jsx
@@ -4,12 +4,12 @@ const Home = React.lazy(() => import("./Home"));
 const Lorem = React.lazy(() => import("./Lorem"));
 
 const Class12 = () => {
-    return (
-        <React.Suspense fallback={<h1> Loading... </h1>}>
-            <Home />
-            <Lorem />
-        </React.Suspense>
-    );
+  return (
+    <React.Suspense fallback={<h1> Loading... </h1>}>
+      <Home />
+      <Lorem />
+    </React.Suspense>
+  );
 };
 
 export default Class12;
