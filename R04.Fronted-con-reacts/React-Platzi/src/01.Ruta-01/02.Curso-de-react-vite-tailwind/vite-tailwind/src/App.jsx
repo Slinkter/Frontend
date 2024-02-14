023cad4e34c19526c47511f8b18caa0672c006e4
Slinkter <luis.j.cueva@gmail.com>
2024-02-14 12:48:39 -0500

    vite push

---
+++ R04.Fronted-con-reacts/React-Platzi/src/01.Ruta-01/02.Curso-de-react-vite-tailwind/vite-tailwind/src/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App = () => {
+  return <div className="bg-zinc-300">hola vite</div>;
+};
+
+export default App;
---
+++ R04.Fronted-con-reacts/React-Platzi/src/01.Ruta-01/02.Curso-de-react-vite-tailwind/vite-tailwind/src/Pages/Components/MyAccont.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MyAccont = () => {
+  return <div>MyAccont</div>;
+};
+
+export default MyAccont;
