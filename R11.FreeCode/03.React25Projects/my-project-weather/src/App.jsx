1459a066e68adcce1dcde0dec8b54e46bd813533
Slinkter <luis.j.cueva@gmail.com>
2024-02-26 19:41:38 -0500

    update

---
+++ R11.FreeCode/03.React25Projects/my-project-weather/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Weather from "./components/weather";
+
+const App = () => {
+  return (
+    <div>
+      <Weather />
+    </div>
+  );
+};
+
+export default App;
---
+++ R11.FreeCode/03.React25Projects/my-project-weather/src/components/weather/index.jsx
@@ -0,0 +1,93 @@
+import React, { useEffect, useState } from "react";
+import Search from "../search";
+import "../../App.css";
+
+const Weather = () => {
+  const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [weatherData, setWeatherData] = useState(null);
+
+  async function fetchWeatherData(params) {
+    try {
+      const api_key = "95f387e6c20dba2ec0661e6dfe0dfd9d";
+      const url_api = `https://api.openweathermap.org/data/2.5/weather?q=${params}&appid=${api_key}`;
+      const res = await fetch(url_api);
+      const data = await res.json();
+      if (data) {
+        setWeatherData(data);
+        setLoading(false);
+      }
+      console.log(data);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  }
+
+  function getCurrentDate() {
+    return new Date().toLocaleDateString("en-us", {
+      weekday: "long",
+      month: "long",
+      day: "numeric",
+      year: "numeric",
+    });
+  }
+
+  useEffect(() => {
+    fetchWeatherData("Lima");
+    return () => {};
+  }, []);
+
+  function handleSearch() {
+    fetchWeatherData(search);
+  }
+
+  return (
+    <div className="">
+      <h1>Weather</h1>
+
+      <Search
+        search={search}
+        setSearch={setSearch}
+        handleSearch={handleSearch}
+      />
+      {loading ? (
+        <div className="loading"> Loading ... </div>
+      ) : (
+        <div className="App">
+          <div className="city-name">
+            <h2>
+              {weatherData?.name} , <span>{weatherData?.sys?.country}</span>
+            </h2>
+          </div>
+          <div className="date">
+            <span>{getCurrentDate()}</span>
+          </div>
+          <div className="temp">{weatherData?.main?.temp}</div>
+          <p className="description">
+            {weatherData && weatherData.weather && weatherData.weather[0]
+              ? weatherData.weather[0].description
+              : ""}
+          </p>
+          <div className="weather-info">
+            <div className="column">
+              <div>
+                <p className="wind"> {weatherData?.wind?.speed}</p>
+                <p>Wind Speed</p>
+              </div>
+            </div>
+
+            <div className="column">
+              <div>
+                <p className="humidity"> {weatherData?.main?.humidity} %</p>
+                <p>Humidy</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Weather;
