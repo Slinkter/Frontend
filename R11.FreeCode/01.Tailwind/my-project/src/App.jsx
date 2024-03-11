f5dbcf5ec53351109c6dae3d6afde1d768e418a2
Slinkter <luis.j.cueva@gmail.com>
2024-03-10 19:32:13 -0500

    update

--- R11.FreeCode/01.Tailwind/my-project/src/App.jsx
+++ R11.FreeCode/01.Tailwind/my-project/src/App.jsx
@@ -4,24 +4,32 @@ import Page from "./page";
 const App = () => {
   return (
     <>
-      {/*     <div className="whitespace-pre-line">
-        <h1 className="text-3xl"> Tittle 1 </h1>
-        <h2 className="text-2xl">tittle 2</h2>
-        <h3 className="text-xl">title 3</h3>
-        <p className="uppercase lowercase text-base leading-normal underline decoration-red-500 decoration-wavy decoration-1 underline-offset-8">
-          A regular paragraph
-        </p>
-        <p className="text-sm">a description paragraph</p>
-        <p className="text-xs note">a little note</p>
-        <p className=" ">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit, qui
-          unde quis numquam facere quas veritatis harum sunt suscipit
-          exercitationem in nam pariatur expedita, natus ipsa omnis nobis eum
-          architecto!
-        </p>
-        <h1></h1>
-      </div> */}
-      <Page />
+      <img
+        className="w-16 md:w-32 lg:w-48"
+        src="https://images.pexels.com/photos/7723354/pexels-photo-7723354.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        alt="img"
+      />
+      <h1 className="text-bluejs text-3xl bg-[#000] ">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, dicta
+        a inventore ducimus id veniam quo distinctio harum esse temporibus quod,
+        repudiandae corrupti ab iure. Fuga expedita dolorem perferendis quae!
+      </h1>
+      <h1 className="text-redjs text-2xl">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, dicta
+        a inventore ducimus id veniam quo distinctio harum esse temporibus quod,
+        repudiandae corrupti ab iure. Fuga expedita dolorem perferendis quae!
+      </h1>
+      <h1 className="text-greenjs-200 text-xl">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, dicta
+        a inventore ducimus id veniam quo distinctio harum esse temporibus quod,
+        repudiandae corrupti ab iure. Fuga expedita dolorem perferendis quae!
+      </h1>
+      <h1 className="text-[#6d28d9] ">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, dicta
+        a inventore ducimus id veniam quo distinctio harum esse temporibus quod,
+        repudiandae corrupti ab iure. Fuga expedita dolorem perferendis quae!
+      </h1>
+      <button className="btn"> Iam a button</button>
     </>
   );
 };
--- R11.FreeCode/01.Tailwind/my-project/tailwind.config.js
+++ R11.FreeCode/01.Tailwind/my-project/tailwind.config.js
@@ -1,15 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        vuejs: {
-          100: "#49e659",
-          200: "#43e659",
+        bluejs: "#1fb6ff",
+        redjs: "#dc2626",
+        greenjs: {
+          100: "#d1fae5",
+          200: "#0d9488",
         },
       },
-      fontSize: {},
     },
   },
   plugins: [],
