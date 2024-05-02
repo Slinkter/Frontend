2035b7ae1d84fedb5af74bc225fcdb8a4bd72f0c
Slinkter <luis.j.cueva@gmail.com>
2024-05-02 13:57:34 -0500

    update

--- R11.FreeCode/05.ReactFirebase/project-react-firebase/src/store/authStore.js
+++ R11.FreeCode/05.ReactFirebase/project-react-firebase/src/store/authStore.js
@@ -1,11 +1,7 @@
 import { create } from "zustand";
 
 const useAuthStore = create((set) => ({
-<<<<<<< HEAD
-  user: null,
-=======
   user: JSON.parse(localStorage.getItem("user-info")),
->>>>>>> 005443a0a62a738b579319a21516f200cea3463c
   login: (user) => set({ user }),
   logout: () => set({ user: null }),
   setUser: (user) => set({ user }),
