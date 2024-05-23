508d8940e9e1557d2e97baeea5b26ff389dbee71
Slinkter <luis.j.cueva@gmail.com>
2024-05-23 18:29:22 -0500

    update

--- R04.Fronted-con-reacts/react_tutorial_interm/src/tutorial02/05.useRef/InputCounter.jsx
+++ R04.Fronted-con-reacts/react_tutorial_interm/src/tutorial02/05.useRef/InputCounter.jsx
@@ -2,45 +2,45 @@ import React, { useEffect, useRef } from "react";
 import { useState } from "react";
 
 const InputCounter = () => {
-    const [text, setText] = useState("");
-    const [count, setCount] = useState(null);
+  const [text, setText] = useState("");
+  const [count, setCount] = useState(null);
 
-    const countRef = useRef(0); // number
-    const inputRef = useRef(null); // element input
-    //
-    const handleIncrementCountRef = () => {
-        setCount(countRef.current++);
-    };
+  const inputRef = useRef(null); // input
+  const countRef = useRef(0); // number
 
-    const handleButton = () => {
-        const input = inputRef.current;
-        input.focus();
-        input.value = text;
-    };
-    //
-    useEffect(() => {
-        console.log("useEffect eject");
-    }, [countRef]);
-    //
-    return (
-        <div>
-            <br />
-            <section>
-                <button onClick={handleIncrementCountRef}> CountRef +1 </button>
-                <p> {count}</p>
-            </section>
-            <br />
-            <section>
-                <input
-                    value={text}
-                    onChange={(e) => setText(e.target.value)}
-                    placeholder="text"
-                    ref={inputRef}
-                />
-                <button onClick={handleButton}>Focus me </button>
-            </section>
-        </div>
-    );
+  const handleIncrementCountRef = () => {
+    setCount(countRef.current++);
+  };
+
+  const handleButton = () => {
+    const input = inputRef.current;
+    input.focus();
+    input.value = text;
+  };
+  //
+  useEffect(() => {
+    console.log("countRef");
+  }, [countRef]);
+  //
+  return (
+    <>
+      <br />
+      <section>
+        <button onClick={handleIncrementCountRef}> CountRef +1 </button>
+        <p> {count}</p>
+      </section>
+      <br />
+      <section>
+        <input
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          placeholder="text"
+          ref={inputRef}
+        />
+        <button onClick={handleButton}>Focus me </button>
+      </section>
+    </>
+  );
 };
 
 export default InputCounter;
