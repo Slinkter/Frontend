0ac496048855079442cbeb56e68f015d4e64b4e4
Slinkter <luis.j.cueva@gmail.com>
2024-07-15 12:25:47 -0500

    update

--- R04.Youtube/05.ReactInstagram/my-app-ig/src/App.jsx
+++ R04.Youtube/05.ReactInstagram/my-app-ig/src/App.jsx
@@ -1,13 +1,34 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { Button } from "@chakra-ui/react";
 import { Route, Routes } from "react-router-dom";
 import PageLayout from "./Layout/PageLayout";
+import {
+    useAuthState,
+    useSignInWithEmailAndPassword,
+    useSignOut,
+} from "react-firebase-hooks/auth";
+import { auth } from "./firebase/firebase";
+import { signOut } from "firebase/auth";
 
 function App() {
-    const [count, setCount] = useState(0);
+    //custom hook
+    const [authUser] = useAuthState(auth);
+    const [signInWithEmailAndPassword] = useSignInWithEmailAndPassword(auth);
+    const [signOut] = useSignOut(auth);
+    const [activeUser, setActiveUser] = useState(null);
+
+    async function handLogout() {
+        await signOut();
+        setActiveUser(null);
+    }
+    async function handLogin() {
+        const response = await signInWithEmailAndPassword(
+            "liam@glass.com",
+            "liamglass"
+        );
+        setActiveUser(response);
+    }
 
     return (
         <>
@@ -18,8 +39,26 @@ function App() {
                     <Route />
                 </Routes>
             </PageLayout>
-            <Button colorScheme="teal" size="sm" variant="outline">
-                Button
+
+            {activeUser && activeUser.user.email}
+            <br />
+
+            <br />
+            <Button
+                colorScheme="teal"
+                size="sm"
+                variant="outline"
+                onClick={handLogin}
+            >
+                Ingresar
+            </Button>
+            <Button
+                colorScheme="teal"
+                size="sm"
+                variant="outline"
+                onClick={handLogout}
+            >
+                Salir
             </Button>
         </>
     );
