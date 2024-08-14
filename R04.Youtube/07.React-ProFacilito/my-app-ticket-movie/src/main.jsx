1038318798c8370c2d08db54f9ae5f901011e81f
Slinkter <luis.j.cueva@gmail.com>
2024-08-14 10:35:25 -0500

    update

--- R04.Youtube/07.React-ProFacilito/my-app-ticket-movie/src/main.jsx
+++ R04.Youtube/07.React-ProFacilito/my-app-ticket-movie/src/main.jsx
@@ -6,15 +6,13 @@ import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
 const config = {
     initialColorMode: "system",
-    useSystemColorMode: false,
+    useSystemColorMode: true,
 };
 
 const theme = extendTheme({ config });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <ChakraProvider theme={theme}>
-            <App />
-        </ChakraProvider>
-    </React.StrictMode>
+    <ChakraProvider theme={theme}>
+        <App />
+    </ChakraProvider>
 );
