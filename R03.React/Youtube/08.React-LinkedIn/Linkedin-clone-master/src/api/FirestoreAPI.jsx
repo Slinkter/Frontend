c75e28dccbbf7f443fa2347a778992732a281264
Slinkter <luis.j.cueva@gmail.com>
2024-09-17 09:00:59 -0500

    update

--- R03.React/Youtube/08.React-LinkedIn/Linkedin-clone-master/src/api/FirestoreAPI.jsx
+++ R03.React/Youtube/08.React-LinkedIn/Linkedin-clone-master/src/api/FirestoreAPI.jsx
@@ -35,132 +35,154 @@ export const postStatus = (object) => {
         });
 };
 
-// Función para obtener todos los posts, ordenados por la marca de tiempo
+// Función para obtener todos los posts,
+// Crea una consulta que ordena los documentos en "posts" por el campo "timeStamp"
+// Escucha cambios en tiempo real de la consulta
 export const getStatus = (setAllStatus) => {
-    const q = query(postsRef, orderBy("timeStamp")); // Crea una consulta que ordena los documentos en "posts" por el campo "timeStamp"
+    const q = query(postsRef, orderBy("timeStamp"));
     onSnapshot(q, (response) => {
-        // Escucha cambios en tiempo real de la consulta
         setAllStatus(
             response.docs.map((docs) => {
-                // Mapea los documentos recibidos en un formato más manejable
-                return { ...docs.data(), id: docs.id }; // Devuelve los datos del documento junto con su ID
+                return { ...docs.data(), id: docs.id };
             })
         );
     });
 };
 
 // Función para obtener todos los usuarios
+// Escucha cambios en tiempo real en la colección "users"
+// Mapea los documentos recibidos en un formato más manejable
+// Devuelve los datos del documento junto con su ID
 export const getAllUsers = (setAllUsers) => {
     onSnapshot(userRef, (response) => {
-        // Escucha cambios en tiempo real en la colección "users"
         setAllUsers(
             response.docs.map((docs) => {
-                // Mapea los documentos recibidos en un formato más manejable
-                return { ...docs.data(), id: docs.id }; // Devuelve los datos del documento junto con su ID
+                return { ...docs.data(), id: docs.id };
             })
         );
     });
 };
 
 // Función para obtener los posts de un usuario específico
+// Crea una consulta para obtener posts con un userID específico
+// Escucha cambios en tiempo real de la consulta
+// Mapea los documentos recibidos en un formato más manejable
+// Devuelve los datos del documento junto con su ID
 export const getSingleStatus = (setAllStatus, id) => {
-    const singlePostQuery = query(postsRef, where("userID", "==", id)); // Crea una consulta para obtener posts con un userID específico
+    const singlePostQuery = query(postsRef, where("userID", "==", id));
     onSnapshot(singlePostQuery, (response) => {
-        // Escucha cambios en tiempo real de la consulta
         setAllStatus(
             response.docs.map((docs) => {
-                // Mapea los documentos recibidos en un formato más manejable
-                return { ...docs.data(), id: docs.id }; // Devuelve los datos del documento junto con su ID
+                return { ...docs.data(), id: docs.id };
             })
         );
     });
 };
 
 // Función para obtener un usuario específico por su correo electrónico
+// Crea una consulta para obtener usuarios con un email específico
+// Escucha cambios en tiempo real de la consulta
+// Mapea los documentos recibidos en un formato más manejable
+// Devuelve los datos del documento junto con su ID
+// Devuelve el primer resultado de la consulta (se espera que solo haya uno)
 export const getSingleUser = (setCurrentUser, email) => {
-    const singleUserQuery = query(userRef, where("email", "==", email)); // Crea una consulta para obtener usuarios con un email específico
+    const singleUserQuery = query(userRef, where("email", "==", email));
     onSnapshot(singleUserQuery, (response) => {
-        // Escucha cambios en tiempo real de la consulta
         setCurrentUser(
             response.docs.map((docs) => {
-                // Mapea los documentos recibidos en un formato más manejable
-                return { ...docs.data(), id: docs.id }; // Devuelve los datos del documento junto con su ID
-            })[0] // Devuelve el primer resultado de la consulta (se espera que solo haya uno)
+                return { ...docs.data(), id: docs.id };
+            })[0]
         );
     });
 };
 
 // Función para agregar datos de usuario a la colección 'users'
+// Agrega un nuevo documento a la colección "users" con los datos del objeto proporcionado
+// No hace nada adicional en caso de éxito
+// Muestra un error en la consola si ocurre algún problema
 export const postUserData = (object) => {
-    addDoc(userRef, object) // Agrega un nuevo documento a la colección "users" con los datos del objeto proporcionado
-        .then(() => {}) // No hace nada adicional en caso de éxito
+    addDoc(userRef, object)
+        .then(() => {})
         .catch((err) => {
-            console.log(err); // Muestra un error en la consola si ocurre algún problema
+            console.log(err);
         });
 };
 
 // Función para obtener los datos del usuario actual, utilizando el correo almacenado en localStorage
+// Escucha cambios en tiempo real en la colección "users"
+// Mapea los documentos recibidos en un formato más manejable
+// Devuelve los datos del documento junto con su ID
+// Filtra los documentos para encontrar el usuario con el correo almacenado en localStorage
+// Devuelve el primer resultado del filtro (se espera que solo haya uno)
 export const getCurrentUser = (setCurrentUser) => {
     onSnapshot(userRef, (response) => {
-        // Escucha cambios en tiempo real en la colección "users"
         setCurrentUser(
             response.docs
                 .map((docs) => {
-                    // Mapea los documentos recibidos en un formato más manejable
-                    return { ...docs.data(), id: docs.id }; // Devuelve los datos del documento junto con su ID
+                    return { ...docs.data(), id: docs.id };
                 })
                 .filter((item) => {
-                    // Filtra los documentos para encontrar el usuario con el correo almacenado en localStorage
                     return item.email === localStorage.getItem("userEmail");
-                })[0] // Devuelve el primer resultado del filtro (se espera que solo haya uno)
+                })[0]
         );
     });
 };
 
 // Función para editar el perfil de un usuario específico
+// Obtiene una referencia al documento del usuario a editar
+// Actualiza el documento del usuario con los datos proporcionados en el payload
+// Muestra una notificación de éxito si el perfil se actualiza correctamente
+// Muestra un error en la consola si ocurre algún problema
 export const editProfile = (userID, payload) => {
-    let userToEdit = doc(userRef, userID); // Obtiene una referencia al documento del usuario a editar
-
-    updateDoc(userToEdit, payload) // Actualiza el documento del usuario con los datos proporcionados en el payload
+    let userToEdit = doc(userRef, userID);
+    updateDoc(userToEdit, payload)
         .then(() => {
-            toast.success("Profile has been updated successfully"); // Muestra una notificación de éxito si el perfil se actualiza correctamente
+            toast.success("Profile has been updated successfully");
         })
         .catch((err) => {
-            console.log(err); // Muestra un error en la consola si ocurre algún problema
+            console.log(err);
         });
 };
 
 // Función para gestionar los likes en los posts
+// Crea una referencia al documento de like usando una combinación de userId y postId
+// Si el post ya está liked, elimina el documento de like
+// Si el post no está liked, crea un nuevo documento de like
+// Muestra un error en la consola si ocurre algún problema
 export const likePost = (userId, postId, liked) => {
     try {
-        let docToLike = doc(likeRef, `${userId}_${postId}`); // Crea una referencia al documento de like usando una combinación de userId y postId
+        let docToLike = doc(likeRef, `${userId}_${postId}`);
         if (liked) {
-            deleteDoc(docToLike); // Si el post ya está liked, elimina el documento de like
+            deleteDoc(docToLike);
         } else {
-            setDoc(docToLike, { userId, postId }); // Si el post no está liked, crea un nuevo documento de like
+            setDoc(docToLike, { userId, postId });
         }
     } catch (err) {
-        console.log(err); // Muestra un error en la consola si ocurre algún problema
+        console.log(err);
     }
 };
 
 // Función para obtener los likes de un post específico y verificar si el usuario actual ha dado like
+// Crea una consulta para obtener los likes de un post específico
+// Escucha cambios en tiempo real de la consulta
+// Mapea los documentos recibidos en un formato más manejable
+// Cuenta el número de likes
+// Verifica si el usuario actual ha dado like
+// Actualiza el estado del contador de likes
+// Actualiza el estado de si el usuario ha dado like o no
+// Muestra un error en la consola si ocurre algún problema
 export const getLikesByUser = (userId, postId, setLiked, setLikesCount) => {
     try {
-        let likeQuery = query(likeRef, where("postId", "==", postId)); // Crea una consulta para obtener los likes de un post específico
-
+        let likeQuery = query(likeRef, where("postId", "==", postId));
         onSnapshot(likeQuery, (response) => {
-            // Escucha cambios en tiempo real de la consulta
-            let likes = response.docs.map((doc) => doc.data()); // Mapea los documentos recibidos en un formato más manejable
-            let likesCount = likes?.length; // Cuenta el número de likes
-
-            const isLiked = likes.some((like) => like.userId === userId); // Verifica si el usuario actual ha dado like
-
-            setLikesCount(likesCount); // Actualiza el estado del contador de likes
-            setLiked(isLiked); // Actualiza el estado de si el usuario ha dado like o no
+            let likes = response.docs.map((doc) => doc.data());
+            let likesCount = likes?.length;
+            const isLiked = likes.some((like) => like.userId === userId);
+            setLikesCount(likesCount);
+            setLiked(isLiked);
         });
     } catch (err) {
-        console.log(err); // Muestra un error en la consola si ocurre algún problema
+        console.log(err);
     }
 };
 
@@ -174,27 +196,29 @@ export const postComment = (postId, comment, timeStamp, name) => {
             name, // Nombre del usuario que hace el comentario
         });
     } catch (err) {
-        console.log(err); // Muestra un error en la consola si ocurre algún problema
+        console.log(err);
     }
 };
 
 // Función para obtener los comentarios de un post específico
-
+// Crea una consulta para obtener comentarios de un post específico
+// Escucha cambios en tiempo real de la consulta
+// Mapea los documentos recibidos en un formato más manejable
+// ID del comentario
+// Datos del comentario
+// Actualiza el estado de los comentarios
 export const getComments = (postId, setComments) => {
     try {
-        let singlePostQuery = query(commentsRef, where("postId", "==", postId)); // Crea una consulta para obtener comentarios de un post específico
-
+        let singlePostQuery = query(commentsRef, where("postId", "==", postId));
+        //
         onSnapshot(singlePostQuery, (response) => {
-            // Escucha cambios en tiempo real de la consulta
             const comments = response.docs.map((doc) => {
-                // Mapea los documentos recibidos en un formato más manejable
                 return {
-                    id: doc.id, // ID del comentario
-                    ...doc.data(), // Datos del comentario
+                    id: doc.id,
+                    ...doc.data(),
                 };
             });
-
-            setComments(comments); // Actualiza el estado de los comentarios
+            setComments(comments);
         });
     } catch (err) {
         console.log(err); // Muestra un error en la consola si ocurre algún problema
@@ -202,24 +226,32 @@ export const getComments = (postId, setComments) => {
 };
 
 // Función para actualizar un post específico
+// Obtiene una referencia al documento del post a actualizar
+// Actualiza el documento del post con el nuevo estado y la nueva imagen
+// Muestra una notificación de éxito si el post se actualiza correctamente
+// Muestra un error en la consola si ocurre algún problema
 export const updatePost = (id, status, postImage) => {
-    let docToUpdate = doc(postsRef, id); // Obtiene una referencia al documento del post a actualizar
     try {
-        updateDoc(docToUpdate, { status, postImage }); // Actualiza el documento del post con el nuevo estado y la nueva imagen
-        toast.success("Post has been updated!"); // Muestra una notificación de éxito si el post se actualiza correctamente
+        let docToUpdate = doc(postsRef, id);
+        updateDoc(docToUpdate, { status, postImage });
+        toast.success("Post has been updated!");
     } catch (err) {
-        console.log(err); // Muestra un error en la consola si ocurre algún problema
+        console.log(err);
     }
 };
 
 // Función para eliminar un post específico
+// Obtiene una referencia al documento del post a eliminar
+// Elimina el documento del post
+// Muestra una notificación de éxito si el post se elimina correctamente
+// Muestra un error en la consola si ocurre algún problema
 export const deletePost = (id) => {
-    let docToDelete = doc(postsRef, id); // Obtiene una referencia al documento del post a eliminar
+    let docToDelete = doc(postsRef, id);
     try {
-        deleteDoc(docToDelete); // Elimina el documento del post
-        toast.success("Post has been Deleted!"); // Muestra una notificación de éxito si el post se elimina correctamente
+        deleteDoc(docToDelete);
+        toast.success("Post has been Deleted!");
     } catch (err) {
-        console.log(err); // Muestra un error en la consola si ocurre algún problema
+        console.log(err);
     }
 };
 
