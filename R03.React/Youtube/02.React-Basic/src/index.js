8171f07ab511c507f4cda739d4c9bf761fbe8e4e
Slinkter <luis.j.cueva@gmail.com>
2024-09-25 10:38:45 -0500

    ok

--- R03.React/Youtube/02.React-Basic/src/index.js
+++ R03.React/Youtube/02.React-Basic/src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import main from "./tutorial/11-redux-toolkit/main";
+import App from "./tutorial/4-useContextMovie/App.js";
+import "./index.css";
+//import main from "./tutorial/11-redux-toolkit/main";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(main);
+root.render(<App />);
