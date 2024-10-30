e4ea6768c60e6a3f55f0ac6a876875ed59955f04
Slinkter <luis.j.cueva@gmail.com>
2024-10-30 12:19:36 -0500

    update

--- R05.NodeJS/parte04 - eventos/app5.js
+++ R05.NodeJS/parte04 - eventos/app5.js
@@ -3,14 +3,53 @@
 
 function ordenarProducto(producto) {
     return new Promise((res, rej) => {
-        console.log(`ordernando : ${producto}`);
+        console.log(`1:ordernando : ${producto}`);
 
         setTimeout(() => {
             if (producto === "taza") {
-                res("ordeando una taza ");
+                res("2.1ordeando una taza ");
             } else {
-                rej("este producto no esta disponible");
+                rej("2.2este producto no esta disponible");
             }
         }, 3000);
     });
 }
+function procesarPedido(rpta) {
+    return new Promise((res, rej) => {
+        console.log("3.procesando respuesta");
+        console.log(`4.la respuesta fue : "${rpta}"`);
+        setTimeout(() => {
+            res("5.Gracias por tu compra");
+        }, 3000);
+    });
+}
+// ejecutar 2 funciones asicronos
+// version then y catch
+/* ordenarProducto("taza")
+    .then((rpta) => {
+        console.log("respuesta recibida");
+        console.log(rpta);
+        return procesarPedido(rpta);
+    })
+    .then((rpta2) => {
+        console.log(rpta2);
+    })
+    .catch((err) => {
+        console.log(err);
+    }); */
+// version async-await
+
+async function realizarPedido(obj) {
+    try {
+        const rpta = await ordenarProducto(obj);
+        const rpta2 = await procesarPedido(rpta);
+        /*  */
+        console.log("respuesta recibida");
+        console.log(rpta);
+        console.log(rpta2);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+realizarPedido("taza");
