cabe6cead6abeeb8190eeeeba13eca567cc8ada6
Slinkter <luis.j.cueva@gmail.com>
2024-11-05 19:05:54 -0500

    update

--- R03.React/Youtube/01.Tailwind/my-project/src/components/Youtube/MainApp.jsx
+++ R03.React/Youtube/01.Tailwind/my-project/src/components/Youtube/MainApp.jsx
@@ -7,7 +7,6 @@ import Footer from "./components/Footer";
 const MainApp = () => {
     return (
         <>
-            <Header />
             <Main />
             <Videos />
             <Footer />
