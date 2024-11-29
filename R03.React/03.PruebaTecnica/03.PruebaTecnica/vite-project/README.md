3268608c309cba89ead582822c6fae8e39a388f6
Luis J Cueva <Slinkter@users.noreply.github.com>
2024-11-29 16:49:26 -0500

    update tecnica 03
    update tecnica 03

---
+++ R03.React/03.PruebaTecnica/03.PruebaTecnica/vite-project/README.md
@@ -0,0 +1,159 @@
+```js
+import { useEffect, useRef, useState } from "react";
+import { User } from "./type/user";
+
+const URL_API_USERRANDOM = "https://randomuser.me/api/?results=100";
+
+const App = () => {
+    // hooks States
+    const [users, setUsers] = useState<User[]>([]);
+    const [isPaintRow, setisPaintRow] = useState(false);
+    const [isOrderByCountry, setisOrderByCountry] = useState(false);
+    const [countrySearch, setCountrySearch] = useState<string>("");
+    //
+    const originuser = useRef<User[]>([]);
+    // hook efect
+    useEffect(() => {
+        getUsers();
+    }, []);
+
+    // funtions
+    const getUsers = async () => {
+        try {
+            const res = await fetch(URL_API_USERRANDOM);
+            if (!res.ok) {
+                throw Error("error en la api");
+            }
+            const data = await res.json();
+            setUsers(data.results);
+            originuser.current = data.results;
+            console.log(res);
+            console.log(data.results);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const handleTogglePaintRows = () => {
+        setisPaintRow(!isPaintRow);
+    };
+
+    const handleToggleOrderByCountry = () => {
+        setisOrderByCountry(!isOrderByCountry);
+
+        if (isOrderByCountry) {
+            setUsers(originuser.current);
+            return;
+        }
+
+        const listsort = [...users].sort((a, b) => {
+            return a.location.country.localeCompare(b.location.country);
+        });
+
+        setUsers(listsort);
+
+        console.log(listsort);
+    };
+    const handleDeleteUserById = (uuid: string) => {
+        const newlist = [...users].filter((u) => u.login.uuid !== uuid);
+        setUsers(newlist);
+    };
+
+    const restoreOriginUsers = () => {
+        setUsers(originuser.current);
+    };
+
+    const usersSorted = isOrderByCountry
+        ? [...users].sort((a, b) => {
+              return a.location.country.localeCompare(b.location.country);
+          })
+        : users;
+
+    const usersRender = usersSorted.filter((u) =>
+        u.location.country.toLowerCase().includes(countrySearch.toLowerCase())
+    );
+
+    return (
+        <div>
+            <h1>Lista de usuarios </h1>
+            <section
+                style={{
+                    margin: "1rem 0",
+                    display: "flex",
+                    gap: "2rem",
+                    justifyContent: "center",
+                }}
+            >
+                <button onClick={handleTogglePaintRows}>Colorear filar</button>
+                <button onClick={handleToggleOrderByCountry}>
+                    Ordenar por pais
+                </button>
+                <button onClick={restoreOriginUsers}>
+                    Restaurar estado inicial
+                </button>
+                <input
+                    type="text"
+                    value={countrySearch}
+                    onChange={(e) => setCountrySearch(e.target.value)}
+                />
+            </section>
+            <table
+                style={{
+                    width: "100%",
+                }}
+                className={isPaintRow ? "colorOddRows" : ""}
+            >
+                <thead
+                    style={{
+                        fontSize: "1.5rem",
+                        color: "white",
+                        background: "black",
+                    }}
+                >
+                    <tr>
+                        <th>foto</th>
+                        <th>Nombre</th>
+                        <th>Apellido</th>
+                        <th> Pais </th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {usersRender.map((u, index) => {
+                        return (
+                            <tr key={u.login.uuid}>
+                                <td>
+                                    <img
+                                        src={u.picture.thumbnail}
+                                        alt={u.name.first}
+                                    />
+                                </td>
+                                <td>{u.name.first}</td>
+                                <td>{u.name.last}</td>
+                                <td>{u.location.country}</td>
+                                <td>
+                                    {index + 1}
+                                    <button
+                                        onClick={() =>
+                                            handleDeleteUserById(u.login.uuid)
+                                        }
+                                    >
+                                        Eliminar
+                                    </button>
+                                </td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+
+            {/*   <pre>{JSON.stringify(listUser.results[0], null, 2)}</pre> */}
+        </div>
+    );
+};
+
+export default App;
+
+
+
+```
--- R03.React/03.PruebaTecnica/03.PruebaTecnica/vite-project/src/App.tsx
+++ R03.React/03.PruebaTecnica/03.PruebaTecnica/vite-project/src/App.tsx
@@ -1,133 +1,29 @@
-import React, { useEffect, useRef, useState } from "react";
-import { schema, TypeApi, User } from "./Type/user";
-const URL_USER = "https://randomuser.me/api/?results=50";
+import React from "react";
+
+const URL_API_USERRANDOM = "https://randomuser.me/api/?results=10";
 
 const App = () => {
-    const [users, setUsers] = useState<User[]>([]);
-    const [isPaintRow, setIsPaintRow] = useState(false);
-    const [isOrderByCountry, setIsOrderByCountry] = useState(false);
-    const [countrySearch, setCountrySearch] = useState("");
-
-    const listOrigin = useRef<User[]>([]);
-
-    useEffect(() => {
-        fethData();
-    }, []);
-
-    const fethData = async () => {
-        try {
-            const res = await fetch(URL_USER);
-            const data: TypeApi = await res.json();
-            const result = schema.safeParse(data);
-
-            if (result.success) {
-                setUsers(result.data.results);
-                listOrigin.current = result.data.results;
-            } else {
-                console.log(result.error);
-            }
-
-            console.log(data);
-            console.log(result);
-        } catch (error) {
-            console.log(error);
-        } finally {
-            console.log("fin");
-        }
-    };
-
-    /* function */
-
-    const btnTogglePaintRows = () => {
-        setIsPaintRow(!isPaintRow);
-    };
-    const btnToggleOrderByCountry = () => {
-        setIsOrderByCountry((prev) => !prev);
-
-        if (isOrderByCountry) {
-            setUsers(listOrigin.current);
-            return;
-        }
-
-        const listsort = [...users].sort((a, b) => {
-            return a.location.country.localeCompare(b.location.country);
-        });
-
-        setUsers(listsort);
-    };
-    const btnDeleteUserById = (id: string) => {
-        const newlist = [...users].filter((u) => u.login.uuid !== id);
-        setUsers(newlist);
-    };
-    const btnRestoreOriginUser = () => {
-        setUsers(listOrigin.current);
-    };
-
-    const userSorted = isOrderByCountry
-        ? users.sort((u1, u2) => {
-              return u1.location.country.localeCompare(u2.location.country);
-          })
-        : users;
-
-    const userRender = userSorted.filter((user) => {
-        return user.location.country
-            .toLowerCase()
-            .includes(countrySearch.toLowerCase());
-    });
-
     return (
         <div>
-            <h1>Tabla de paises</h1>
-            <section className="main-section ">
-                <button onClick={btnTogglePaintRows}>colorear fila</button>
-                <button onClick={() => btnToggleOrderByCountry()}>
-                    ordernar por pais
-                </button>
-                <button onClick={() => btnRestoreOriginUser()}>
-                    restaurar estado inicial{" "}
-                </button>
-                <input
-                    className="text-cyan-600"
-                    type="text"
-                    value={countrySearch}
-                    onChange={(e) => setCountrySearch(e.target.value)}
-                />
+            App
+            <section>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Foto</th>
+                            <th>Nombre</th>
+                            <th>Apellido</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>img</td>
+                            <td>liam</td>
+                            <td>cave</td>
+                        </tr>
+                    </tbody>
+                </table>
             </section>
-
-            <table className={isPaintRow ? "paintRow" : ""}>
-                <thead className="bg-slate-200">
-                    <tr>
-                        <th>Foto</th>
-                        <th>Nombre</th>
-                        <th>Apellido</th>
-                        <th>Pais</th>
-                        <th>Acciones</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {userRender.map((user, index) => {
-                        return (
-                            <tr key={index}>
-                                <td>
-                                    <img src={user.picture.thumbnail} alt="" />
-                                </td>
-                                <td>{user.name.first}</td>
-                                <td>{user.name.last}</td>
-                                <td>{user.location.country}</td>
-                                <td>
-                                    <button
-                                        onClick={() =>
-                                            btnDeleteUserById(user.login.uuid)
-                                        }
-                                    >
-                                        Eliminar
-                                    </button>
-                                </td>
-                            </tr>
-                        );
-                    })}
-                </tbody>
-            </table>
         </div>
     );
 };
