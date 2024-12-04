29bbd35f8ea2bc500ea47a048be71e885c267a19
Luis J Cueva <Slinkter@users.noreply.github.com>
2024-12-04 10:21:02 -0500

    macos

--- R02.Javascript/04.Curso-de-manipulacion-de-arrays-fin/App/01-for-each.js
+++ R02.Javascript/04.Curso-de-manipulacion-de-arrays-fin/App/01-for-each.js
@@ -1,9 +1,9 @@
-// for old
+const lettersNew = ["z", "x", "y"];
+lettersNew.forEach((element) => console.log(element));
+
+/* 
 const lettersOld = ["a", "b", "c"];
 for (let index = 0; index < lettersOld.length; index++) {
     const element = lettersOld[index];
     console.log(element);
-}
-// for new
-const lettersNew = ["z", "x", "y"];
-lettersNew.forEach((element) => console.log(element));
+} */
