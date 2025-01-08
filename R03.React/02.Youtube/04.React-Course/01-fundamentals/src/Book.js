0e6de6c64bb793cd2ff52b7b3d3a7355a3cd38eb
Luis J Cueva <Slinkter@users.noreply.github.com>
2025-01-07 21:18:58 -0500

    ok
    ok

--- R03.React/02.Youtube/04.React-Course/01-fundamentals/src/Book.js
+++ R03.React/02.Youtube/04.React-Course/01-fundamentals/src/Book.js
@@ -1,14 +1,13 @@
 const Book = (props) => {
-  const { img, title, author } = props;
+    const { img, title, author } = props;
 
-  return (
-    <article className='book'>
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-
-      <h4>{author} </h4>
-    </article>
-  );
+    return (
+        <article className="book">
+            <img src={img} alt={title} />
+            <h2>{title}</h2>
+            <h4>{author} </h4>
+        </article>
+    );
 };
 
 export default Book;
