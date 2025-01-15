75ff20394e48a1269ec326edf95094fc06c488a4
Luis J Cueva <Slinkter@users.noreply.github.com>
2025-01-14 20:25:41 -0500

    update

--- R03.React/02.Youtube/04.React-Course/04-fundamental-projects/02-tours/starter/src/components/Tours.jsx
+++ R03.React/02.Youtube/04.React-Course/04-fundamental-projects/02-tours/starter/src/components/Tours.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Tour from "./Tour";
+
 const Tours = ({ tours, removeTour }) => {
     return (
         <section>
             <div className="title">
-                <h2>our tours</h2>
+                <h2>Our Tours</h2>
                 <div className="title-underline"></div>
             </div>
             <div className="tours">
