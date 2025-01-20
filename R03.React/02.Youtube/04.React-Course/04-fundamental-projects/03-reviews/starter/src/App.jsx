108527cba6a417ed8cae59587146771158a2f4eb
Luis J Cueva <Slinkter@users.noreply.github.com>
2025-01-20 18:06:58 -0500

    ok
    ok macos

--- R03.React/02.Youtube/04.React-Course/04-fundamental-projects/03-reviews/starter/src/App.jsx
+++ R03.React/02.Youtube/04.React-Course/04-fundamental-projects/03-reviews/starter/src/App.jsx
@@ -1,4 +1,69 @@
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
+import reviews from "./data";
+import { useState } from "react";
+
+console.log(reviews);
+
 const App = () => {
-  return <h2>Reviews Starter</h2>;
+    const [index, setIndex] = useState(0);
+
+    const { id, image, job, name, text } = reviews[index];
+
+    const nextPerson = () => {
+        setIndex((prev) => {
+            let newindex = prev + 1;
+            return auxCheckNumber(newindex);
+        });
+    };
+    const prevPerson = () => {
+        setIndex((prev) => {
+            let newindex = prev - 1;
+            return auxCheckNumber(newindex);
+        });
+    };
+    const randomPerson = () => {
+        let random = Math.floor(Math.random() * reviews.length);
+        random === index ? random + 1 : random;
+        setIndex(auxCheckNumber(random));
+    };
+
+    const auxCheckNumber = (num) => {
+        if (num > reviews.length - 1) {
+            return 0;
+        }
+        if (num < 0) {
+            return reviews.length - 1;
+        }
+        return num;
+    };
+
+    console.log(reviews[index]);
+
+    return (
+        <main>
+            <article className="review">
+                <div className="img-container">
+                    <img src={image} alt="" className="person-img" />
+                    <span className="quote-icon">
+                        <FaQuoteRight />
+                    </span>
+                </div>
+                <h4 className="author">{name}</h4>
+                <p className="job">{job}</p>
+                <p className="info">{text}</p>
+                <div className="btn-container">
+                    <button className="prev-btn" onClick={prevPerson}>
+                        <FaChevronLeft />
+                    </button>
+                    <button className="next-btn" onClick={nextPerson}>
+                        <FaChevronRight />
+                    </button>
+                </div>
+                <button className="btn btn-hispter" onClick={randomPerson}>
+                    surprise me
+                </button>
+            </article>
+        </main>
+    );
 };
 export default App;
