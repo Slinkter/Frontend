f359371bba4f2b7b4a7e6e9e0103633237ae5f3f
Luis J Cueva <Slinkter@users.noreply.github.com>
2025-02-24 12:25:03 -0500

    ok

--- R03.React/02.Youtube/04.React-Course/04-fundamental-projects/14-cart/starter/src/utils.js
+++ R03.React/02.Youtube/04.React-Course/04-fundamental-projects/14-cart/starter/src/utils.js
@@ -1,10 +1,10 @@
 export const getTotals = (cart) => {
-    let totalAmount = 0;
-    let totalCost = 0;
+  let totalAmount = 0;
+  let totalCost = 0;
 
-    for (let { amount, price } of cart.values()) {
-        totalAmount += amount;
-        totalCost += amount * price;
-    }
-    return { totalAmount, totalCost };
+  for (let { amount, price } of cart.values()) {
+    totalAmount += amount;
+    totalCost += amount * price;
+  }
+  return { totalAmount, totalCost };
 };
