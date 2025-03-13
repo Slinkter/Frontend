d502a18c07cab0008fff1df13121697b328ca38b
Luis J Cueva <Slinkter@users.noreply.github.com>
2025-03-13 10:47:38 -0500

    update

--- R03.React/02.Youtube/01.React-Course/11-redux-toolkit-tutorial/vite-starter/src/slice/cartSlice.js
+++ R03.React/02.Youtube/01.React-Course/11-redux-toolkit-tutorial/vite-starter/src/slice/cartSlice.js
@@ -1,22 +1,41 @@
 import { createAsyncThunk, createSlice, createSlice } from "@reduxjs/toolkit";
-const initialState = {
-  isLoading: true,
-  cartItem: [],
-  amount: 0,
-  total: 0,
-};
-/* middle */
+
 const getCartItems = createAsyncThunk();
 
 const createSlice = createSlice({
   name: "cart",
-  initialState: initialState,
+  initialState: {
+    isLoading: true,
+    cartItem: [],
+    amount: 0,
+    total: 0,
+  },
   reducer: {
-    clearCart: (state, action) => {},
-    removeItem: (state, action) => {},
-    increase: (state, action) => {},
-    decrease: (state, action) => {},
+    clearCart: (state, action) => {
+      state.cartItem = [];
+    },
+    removeItem: (state, action) => {
+      const id = action.payload;
+      state.cartItem = state.cartItem.filter((item) => item.id !== id);
+    },
+    increase: (state, action) => {
+      const id = action.payload;
+      state.cartItem = state.cartItem.find((item) => item.id === id);
+      cartItem.amount += 1;
+    },
+    decrease: (state, action) => {
+      const id = action.payload;
+      state.cartItem = state.cartItem.find((item) => item.id === id);
+      cartItem.amount -= 1;
+    },
     calculateTotals: (state, action) => {},
   },
-  extraReducers: (builer) => {},
+  extraReducers: (builer) => {
+    builer
+      .addCase(getCartItems.pending, (state, action) => {
+        state.isLoading = true;
+      })
+      .addCase(getCartItems.fulfilled, (state, action) => {})
+      .addCase(getCartItems.rejected, (state, action) => {});
+  },
 });
