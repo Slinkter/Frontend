8e5a835c421ecce609f773f10d7daaeae170b399
Luis J Cueva <Slinkter@users.noreply.github.com>
2025-04-06 12:14:48 -0500

    macos

--- R03.React/02.Youtube/01.React-Course/13-typescript-tutorial/src/interface.ts
+++ R03.React/02.Youtube/01.React-Course/13-typescript-tutorial/src/interface.ts
@@ -1,30 +1,18 @@
-let unknownValue: unknown
-unknownValue = "Hello world"
-unknownValue = [1, 2, 3]
-unknownValue = 43.12312
-console.log(unknownValue);
-
-if (typeof unknownValue === "number") {
-  console.log(unknownValue.toFixed(2));
+enum Color {
+  Red, Blue,
+  //Green,
 }
 
-function runSomeCode() {
-  const random = Math.random()
-  if (random < 0.5) {
-    throw new Error("something went wrong")
-  } else {
-    throw "some error"
+function getColorName(color: Color) {
+  switch (color) {
+    case Color.Red:
+      return "Red"
+    case Color.Blue:
+      return "Blue"
+    default:
+      let unexpectedColor: never = color
+      throw new Error(`unexpected color value :   ${unexpectedColor}`)
   }
 }
 
-
-try {
-  runSomeCode()
-} catch (error) {
-  if (error instanceof Error) {
-    console.log(error.message);
-  } else {
-    console.log(error);
-
-  }
-}
+console.log(getColorName(Color.Red));
