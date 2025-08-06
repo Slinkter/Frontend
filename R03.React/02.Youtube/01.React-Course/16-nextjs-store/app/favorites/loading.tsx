f88c0669d0a1a17055f558b52e9040318857b621
Luis J Cueva <Slinkter@users.noreply.github.com>
2025-08-06 12:28:57 -0500

    windows 10

--- R03.React/02.Youtube/01.React-Course/16-nextjs-store/app/favorites/loading.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import LoadingContainer from '@/components/global/LoadingContainer';
-
-function loading() {
-  return <LoadingContainer />;
-}
-export default loading;
--- R03.React/02.Youtube/01.React-Course/16-nextjs-store/app/orders/loading.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import LoadingTable from '@/components/global/LoadingTable';
-
-function loading() {
-  return <LoadingTable />;
-}
-export default loading;
