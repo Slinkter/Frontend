e366bdb820d05d062000b7f336666193deaa295d
Luis J Cueva <Slinkter@users.noreply.github.com>
2026-01-22 12:28:59 -0500

    ok
    ok

---
+++ R04.Typescript/typescript03/src/action.ts
@@ -0,0 +1,17 @@
+export function sayHello(name: string): void {
+    console.log(` heloe ${name}`);
+}
+
+export let person = "susan";
+
+type Student = {
+    name: string;
+    age: number;
+};
+
+const newStuden: Student = {
+    name: "peter",
+    age: 1,
+};
+
+export default newStuden;
