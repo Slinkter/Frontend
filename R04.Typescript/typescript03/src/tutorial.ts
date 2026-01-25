5199c70075da83b74e79cba04ea41a3117885990
Luis J Cueva <Slinkter@users.noreply.github.com>
2026-01-25 16:40:33 -0500

    typescript
    typescript

--- R04.Typescript/typescript03/src/tutorial.ts
+++ R04.Typescript/typescript03/src/tutorial.ts
@@ -1,1 +1,21 @@
-import bcrypt from "bcryptjs";
+interface IPerson {
+    name: string;
+    age: number;
+    greet(): void;
+}
+
+class Person implements IPerson {
+    public name: string;
+    public age: number;
+    constructor(p1: string, p2: number) {
+        this.name = p1;
+        this.age = p2;
+    }
+
+    greet(): void {
+        console.log("hola", "", "name ", this.name, " age :", this.age);
+    }
+}
+
+const person01 = new Person("luis", 23);
+person01.greet();
