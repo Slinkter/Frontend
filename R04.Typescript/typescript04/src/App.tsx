3afee58a29c9acf1d189d8e1090b7cf2dc1e7e31
Luis J Cueva <Slinkter@users.noreply.github.com>
2026-01-26 12:11:30 -0500

    ok
    ok

--- R04.Typescript/typescript04/src/App.tsx
+++ R04.Typescript/typescript04/src/App.tsx
@@ -1,4 +1,4 @@
-import Component from "./starter/03-state";
+import Component from "./starter/04-events";
 
 function App() {
     return (
--- R04.Typescript/typescript04/src/starter/04-events/index.tsx
+++ R04.Typescript/typescript04/src/starter/04-events/index.tsx
@@ -1,9 +1,48 @@
+import { useState } from "react";
+
+type Person = {
+    name: string;
+    email: string;
+};
+
 function Component() {
-  return (
-    <div>
-      <h2>React & Typescript</h2>
-      <h2>Events</h2>
-    </div>
-  );
+    const [text, setText] = useState("");
+    const [email, setEmail] = useState("");
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(e.target.value);
+        setEmail(e.target.value);
+    };
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+        const data = Object.fromEntries(formData);
+        console.log(data);
+    };
+
+    return (
+        <div>
+            <h2>React & Typescript</h2>
+            <h2>Events</h2>
+            <form className="form" onSubmit={(e) => handleSubmit(e)}>
+                <input
+                    type="text"
+                    className="form-input mb-1"
+                    value={text}
+                    onChange={(e) => setText(e.target.value)}
+                />
+                <input
+                    type="email"
+                    className="form-input mb-1"
+                    value={email}
+                    onChange={(e) => handleChange(e)}
+                />
+                <button type="submit" className="btn btn-block">
+                    submit
+                </button>
+            </form>
+        </div>
+    );
 }
 export default Component;
